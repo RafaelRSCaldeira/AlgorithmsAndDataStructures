0e0760d9d7e764e8c44d767e79b6eff21f51c57b
Rafael Caldeira <rafael.caldeira.2005@gmail.com>
2024-08-09 19:37:02 -0300

    chore

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {1, 2};
-        testPointer(arr);
-        testPointer2(arr);
-        for(int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
-        }
-    }
-
-    private static void testPointer(int[] arr) {
-        arr[0] = 10;
-        arr[1] = 20;
-        return;
-    }
-
-    private static void testPointer2(int[] arr) {
-        int[] arr2 = {0, 1, 2, 3, 4, 5};
-        arr2[0] = -1;
-        return;
+        Sort merge = new MergeSort();
+        int[] arr = {10, 7, 1, 8, 5, 0};
+        merge.sort(arr);
     }
 }
